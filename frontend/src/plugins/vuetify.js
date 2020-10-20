39a374a8a3cc7ddcf9233a49b6b511a2b45e05c0
Jiyoon-park <soundpjy1205@gmail.com>
2020-10-20 14:48:16 +0900

    resolves S03P31A408-8 | [chore] Add vuetify preset

--- frontend/src/plugins/vuetify.js
+++ frontend/src/plugins/vuetify.js
@@ -1,7 +1,21 @@
 import Vue from 'vue';
 import Vuetify from 'vuetify/lib';
+import { preset } from 'vue-cli-plugin-vuetify-preset-fortnightly/preset'
 
 Vue.use(Vuetify);
+const options = {
+  breakpoint: { scrollbarWidth: 12 },
+  theme: {
+    themes: {
+      light: { primary: '#01579b' },
+    },
+  },
+}
 
 export default new Vuetify({
-});
+  // The provided global preset is first merged with defaults
+  preset,
+  // User options are then merged, overwriting defaults and the global preset
+  ...options,
+})
+
