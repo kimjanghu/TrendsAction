48d87bca47b400ffd512ac0db1740d3b9550f99f
김동휘 <wheeking@gmail.com>
2020-10-20 16:40:21 +0900

    Resolves S03P31A408-9 | [feat] Add board list lookup

--- backend/trends/src/main/java/com/action/trends/controller/BoardController.java
+++ backend/trends/src/main/java/com/action/trends/controller/BoardController.java
@@ -1,13 +1,16 @@
 package com.action.trends.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.action.trends.dto.Board;
@@ -24,11 +27,37 @@ public class BoardController {
 	
 	@ApiOperation(value="보드 리스트 조회", response = List.class)
 	@GetMapping("/board/{userId}")
-	public @ResponseBody List<Board> getBoardList(@PathVariable int userId) {
+	public ResponseEntity<Map<String, Object>> getBoardList(@PathVariable int userId) {
+		ResponseEntity<Map<String, Object>> entity = null;
 		List<Board> list = new ArrayList<Board>();
-		list = boardService.getBoardList(userId);
+		Map<String, Object> resultMap = new HashMap<>();
 		
-		return list;
+		try {
+			list = boardService.getBoardList(userId);
+			resultMap.put("status", true);
+			resultMap.put("message", "보드 리스트 조회에 성공했습니다.");
+			resultMap.put("data", list);
+			entity = new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+		} catch (RuntimeException e) {
+			entity = handleException(e);
+		}
+		
+		return entity;
 	}
 	
+	// ------------------- 예외처리 -----------------------
+	
+	private ResponseEntity<Map<String, Object>> handleSuccess(Object data) {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("status", true);
+		resultMap.put("message", data);
+		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.OK);
+	}
+
+	private ResponseEntity<Map<String, Object>> handleException(Exception e) {
+		Map<String, Object> resultMap = new HashMap<>();
+		resultMap.put("status", false);
+		resultMap.put("message", e.getMessage());
+		return new ResponseEntity<Map<String, Object>>(resultMap, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 }
