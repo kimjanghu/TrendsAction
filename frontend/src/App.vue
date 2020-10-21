15ca1bd3b66df2e0482687ed15059b7fb6a41514
kimjanghu <kjh921030@gmail.com>
2020-10-21 11:29:03 +0900

    S03P31A408-47 | [feat] Add main page

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,56 +1,30 @@
 <template>
   <v-app>
-    <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar>
-
-    <v-main>
-      <HelloWorld/>
+    <div>
+      <Navbar />
+    </div>
+    <Header />
+    <v-main class="main-area">
+      <router-view 
+        :key="$route.fullPath"
+      ></router-view>
     </v-main>
+    <Footer />
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Navbar from '@/components/common/Navbar'
+import Header from '@/components/common/Header'
+import Footer from '@/components/common/Footer'
 
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+    Navbar,
+    Header,
+    Footer
   },
 
   data: () => ({
@@ -58,3 +32,10 @@ export default {
   }),
 };
 </script>
+
+<style scoped>
+.main-area {
+  width: 85%;
+  margin: 0 auto;
+}
+</style>
