04b9885da436f0159ecfb5226c2ab1bcad69d4f0
kimjanghu <kjh921030@gmail.com>
2020-10-22 00:28:32 +0900

    S03P31A408-47 | [feat] Move header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -3,7 +3,6 @@
     <div>
       <Navbar />
     </div>
-    <Header />
     <v-main class="main-area">
       <router-view 
         :key="$route.fullPath"
@@ -15,7 +14,6 @@
 
 <script>
 import Navbar from '@/components/common/Navbar'
-import Header from '@/components/common/Header'
 import Footer from '@/components/common/Footer'
 
 export default {
@@ -23,7 +21,6 @@ export default {
 
   components: {
     Navbar,
-    Header,
     Footer
   },
 
@@ -34,8 +31,8 @@ export default {
 </script>
 
 <style scoped>
-.main-area {
+/* .main-area {
   width: 85%;
   margin: 0 auto;
-}
+} */
 </style>
--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,26 +1,39 @@
 <template>
-  <div class="main-area">
-    <div>
+  <div>
+    <Header />
+    <div class="main-area d-flex">
       <Carousel />
+      <MainList />
     </div>
-    
-    
+    <v-container>
+      
+    </v-container>
   </div>
 </template>
 
 <script>
 import Carousel from '@/components/common/Carousel'
+import MainList from '@/components/common/MainList'
+import Header from '@/components/common/Header'
 
 export default {
   name: 'Home',
   components: {
-    Carousel
+    Carousel,
+    MainList,
+    Header
+  },
+  data() {
+    return {
+      
+    }
   }
 }
 </script>
 
 <style scoped>
 .main-area {
-  margin: 100px auto;
+  margin: 20px auto;
+  width: 85%;
 }
 </style>
