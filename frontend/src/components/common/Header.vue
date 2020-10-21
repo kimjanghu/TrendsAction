5f3c538cdc384388aaf99af3275f5ecf2e6ef73f
kimjanghu <kjh921030@gmail.com>
2020-10-21 11:32:20 +0900

    S03P31A408-49 | [feat] Add main area

---
+++ frontend/src/components/common/Header.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="header-area">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style>
+.header-area {
+  height: 550px;
+  background-image: url('https://source.unsplash.com/random');
+  background-size: cover;
+}
+</style>
