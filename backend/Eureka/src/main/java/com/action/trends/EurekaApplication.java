93f949d65efacc61cd41cb046b31d88aec3c8286
JaebinChoi <choijaebin13@gmail.com>
2020-10-26 14:08:05 +0900

    S03P31A408-73|[feat] Add eureka server

--- backend/Eureka/src/main/java/com/action/trends/EurekaApplication.java
+++ backend/Eureka/src/main/java/com/action/trends/EurekaApplication.java
@@ -2,14 +2,14 @@ package com.action.trends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaClient
+@EnableEurekaServer
 @SpringBootApplication
-public class TrendApplication {
+public class EurekaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(TrendApplication.class, args);
+		SpringApplication.run(EurekaApplication.class, args);
 	}
 
 }
