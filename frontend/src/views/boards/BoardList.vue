af9738028ab992c77798c0c3947d55e0bcff2213
Jiyoon-park <soundpjy1205@gmail.com>
2020-10-28 12:00:57 +0900

    S03P31A408-66 | [feat] Update my board layout

--- frontend/src/views/boards/BoardList.vue
+++ frontend/src/views/boards/BoardList.vue
@@ -4,11 +4,19 @@
     color="grey lighten-3"
   >
     <v-row>
-      <v-col cols="12" md="6" v-for="n in 4" :key="n">
+      <v-col cols="12" md="4" v-for="(card,n) in cards" :key="n">
         <v-card 
           color="white" 
           :height="$vuetify.breakpoint.smAndDown? 250:200" 
           :to="{name: 'BoardDetail',}">
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+            <v-card-title v-text="card.title"></v-card-title>
+          </v-img>
         </v-card>
       </v-col>
     </v-row>
@@ -17,6 +25,16 @@
 
 <script>
 export default {
+  data: () => ({
+    cards: [
+      { title: '자율프로젝트', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg' },
+      { title: '꼴라쥬', src: 'https://cdn.vuetifyjs.com/images/cards/road.jpg' },
+      { title: 'WEB', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+      { title: '반응형 웹페이지', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+      { title: '애니메이션', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+      { title: '히어로', src: 'https://cdn.vuetifyjs.com/images/cards/plane.jpg' },
+    ],
+  }),
 
 }
 </script>
---
+++ frontend/src/views/users/UserProfileUpdate.vue
@@ -0,0 +1,58 @@
+<template>
+  <v-dialog
+    v-model="dialog"
+    persistent
+    max-width="400px"
+  >
+    <template v-slot:activator="{ on, attrs }">
+      <v-btn dark class="mt-2 custom-rounded" width="100%" color="teal" v-bind="attrs" v-on="on">내 프로필 변경</v-btn>
+    </template>
+    <v-card class="custom-rounded">
+      <v-card-title>
+        <span>내정보 변경</span>
+        <v-spacer></v-spacer>
+        <i class="fas fa-times" @click="dialog = false" style="cursor:pointer;"></i>
+      </v-card-title>
+      <v-card-text class="pt-5 pb-0">
+        <v-text-field
+          label="닉네임"
+          placeholder="닉네임을 입력해주세요"
+          outlined
+          class="px-0 py-0 mb-0 custom-rounded"
+        ></v-text-field>
+        <v-combobox
+          class="custom-rounded"
+          v-model="select"
+          :items="items"
+          label="관심 카테고리"
+          multiple
+          outlined
+        ></v-combobox>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn
+          text
+          @click="dialog = false"
+        >
+          수정
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false,
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
