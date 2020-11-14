0e786197f2052d017bd245d6e8e76c5cd5b74957
Jiyoon-park <soundpjy1205@gmail.com>
2020-11-14 13:55:31 +0900

    S03P31A408-167 | [chore] user profile / design

--- frontend/src/views/users/UserProfileLayout.vue
+++ frontend/src/views/users/UserProfileLayout.vue
@@ -1,12 +1,11 @@
 <template>
-<div>
+<div class="wrapper">
   <Navbar />
   <v-container class="mt-16">
     <v-row justify="center">
       <v-col cols="12" md ="4" lg="3">
         <v-sheet
-          class="d-flex flex-column justify-center align-center"
-          color="teal lighten-3"
+          class="d-flex flex-column justify-center align-center neumor-design"
           :height="$vuetify.breakpoint.smAndDown? 300:330"
         >
           <v-avatar
@@ -18,17 +17,20 @@
             >
           </v-avatar>
           
-          <span class="mt-3">{{ nickname }}님의 마이페이지</span>
+          <span class="mt-5 font-weight-bold" style="font-size: 1.1rem;">{{ nickname }}</span>
           <span>{{ email }}</span>
-          <div v-if="categoryName">
-            {{ categoryName }}
-            <!-- <div v-for="category in categoryList" :key="category"  class="d-flex justify-center">
-              <span>{{ category }}</span>
-            </div> -->
+          <div v-if="categoryName" class="mt-5">
+            <span class="my-favorite" v-for="(category, i) in categoryName" :key="i">
+              {{ category }}
+            </span>
           </div>
         </v-sheet>
-        <v-btn dark class="mt-2 custom-rounded" width="100%" color="teal" :to="{ name: 'BoardList', params: { id: userId } }">내 보드</v-btn>
-        <v-btn dark class="mt-2 custom-rounded" width="100%" color="teal" :to="{ name: 'UserProfile' }">내 프로필 수정하기</v-btn>
+        <router-link :to="{ name: 'BoardList', params: { id: userId } }">
+          <input type="button" value="나의 트렌드 보드" class="neumor-design customBtn">
+        </router-link>
+        <router-link :to="{ name: 'UserProfile' }">
+          <input type="button" value="나의 정보 수정" class="neumor-design customBtn">
+        </router-link>
       </v-col>
       
       <v-col cols="12" md="8" lg="9" >
@@ -55,7 +57,7 @@ export default {
       email: '',
       nickname: '',
       profile: '',
-      categoryName: '',
+      categoryName: [],
       userId: window.localStorage.getItem('userId'),
     }
   },
@@ -68,10 +70,10 @@ export default {
   created() {
     this.getUserInfo()
       .then(data => {
+        console.log(data)
         this.email = data.email
         this.nickname = data.nickname
-        const categoryName = data.categoryName.join(', ')
-        this.categoryName = categoryName
+        this.categoryName = data.categoryName
         this.profile = data.profile
       })
   },
@@ -82,9 +84,33 @@ export default {
 </script>
 
 <style scoped>
+.wrapper {
+  min-height: 100vh;
+}
 .custom-rounded {
   border-radius: 4px;
 }
+
+.customBtn {
+  color:black; 
+  width:100%; 
+  height: 50px;
+  margin-top: 1rem;
+}
+
+.customBtn:hover {
+  border: none;
+  border-radius: 25px;
+  background: #F5F5F6;
+  box-shadow: inset 11px 11px 22px #e4e4e5, 
+              inset -11px -11px 22px #ffffff;
+  cursor: pointer;
+}
+
+.customBtn:focus {
+  outline: none;
+}
+
 
 .header {
   background-color: #777777;
@@ -95,6 +121,23 @@ export default {
 .main-section {
   background-color:#efefef;
   width: 100%;
+}
+
+.neumor-design {
+  padding: 2%;
+  border-radius: 19px;
+  background: #F5F5F6;
+  box-shadow:  9px 9px 18px #ebebec, 
+              -9px -9px 18px #ffffff;
+}
+
+.my-favorite {
+  margin-right: 2px;
+  border: 2px solid rgb(226, 226, 226);
+  border-radius: 10px;
+  background-color: rgb(226, 226, 226);
+  padding: 2px 5px;
+  text-align: center;
 }
 
 </style>
